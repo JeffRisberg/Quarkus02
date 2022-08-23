43ee8ea3ac8e1b5e3b466eff4d9bc2ab1bf3a08b
JeffRisberg <j.s.risberg@gmail.com>
2022-08-22 22:46:08 -0700

    set up gradle configuration so that IDE compiles the code.

--- src/main/java/com/company/Bot.java
+++ src/main/java/com/company/Bot.java
@@ -5,39 +5,37 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-/**
- * Example JPA entity.
- */
+/** Example JPA entity. */
 @Entity
-@Table(name="bots")
+@Table(name = "bots")
 public class Bot {
-    private Long id;
-    private String name;
-    private String description;
+  private Long id;
+  private String name;
+  private String description;
 
-    @Id
-    @GeneratedValue
-    public Long getId() {
-        return id;
-    }
+  @Id
+  @GeneratedValue
+  public Long getId() {
+    return id;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  public void setDescription(String description) {
+    this.description = description;
+  }
 }
--- src/main/java/com/company/DataSource.java
+++ src/main/java/com/company/DataSource.java
@@ -5,30 +5,28 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-/**
- * JPA entity.
- */
+/** JPA entity. */
 @Entity
-@Table(name="data_sources")
+@Table(name = "data_sources")
 public class DataSource {
-    private Long id;
-    private String name;
+  private Long id;
+  private String name;
 
-    @Id
-    @GeneratedValue
-    public Long getId() {
-        return id;
-    }
+  @Id
+  @GeneratedValue
+  public Long getId() {
+    return id;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 }
